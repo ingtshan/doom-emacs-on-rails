b6d1c01760a0d8fc0d21fc76ae4a8830401ac578
ingtshan <rongcanyo@gmail.com>
2022-01-15 22:50:46 +0800

    feature(extends,new config) first commit

--- config.el
+++ config.el
@@ -1,6 +1,20 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+;; to for add load-path recursion like -r
+(unless (boundp 'add-subdirs-to-load-path)
+  (defun add-subdirs-to-load-path(dir)
+    "Recursive add directories to `load-path`."
+    (let ((default-directory (file-name-as-directory dir)))
+      (add-to-list 'load-path dir)
+      (normal-top-level-add-subdirs-to-load-path))))
+
+(add-subdirs-to-load-path
+ (expand-file-name "extends" doom-private-dir))
+
 (if (not (file-exists-p "~/.doom.d/doom-settings.el"))
     (org-babel-load-file
      (expand-file-name "doom-settings.org" doom-private-dir))
   (load (expand-file-name "doom-settings.el" doom-private-dir)))
+
+;; load ingtshan's configuration
+(load (expand-file-name "extends.el" doom-private-dir))
---
+++ extends.el
@@ -0,0 +1,11 @@
+;;; extends.el --- add ingtshang's configuration as extends  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  ingtshan
+
+;; Author: ingtshan <rongcanyo@gmail.com>
+
+;; just like old emacs init.el
+;; for develop and test my package quickly in simple way
+
+;; my package
+(require 'mac-command)
